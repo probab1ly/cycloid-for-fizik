6ee486984eec4b5899a457b2b1ea35c4cbc67998
Дмитрий <142750962+probab1ly@users.noreply.github.com>
2023-11-08 19:29:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,81 +14,83 @@ dtheta = pi / 25;
 
 
 R = float(input('Radius:'))
+if R>0 and R<8:#проверка на радиус
+    figu = plt.figure()
+    ax = plt.axes()
 
-figu = plt.figure()
-ax = plt.axes()
+    figu.set_figheight(7)  # устанавливает высоту фигуры figu
+    figu.set_figwidth(14)  # устанавливает ширину фигуры figu
 
-figu.set_figheight(7)# устанавливает высоту фигуры figu
-figu.set_figwidth(14)#устанавливает ширину фигуры figu
+    xdata, ydata = [], []
+    trace = Line2D([], [], color='blue', linewidth=3)
+    ax.add_line(trace)
 
-xdata, ydata = [], []
-trace = Line2D([], [], color='blue', linewidth=3)
-ax.add_line(trace)
+    rad = Line2D([0, 0], [0, -R], color='red', linewidth=3)
+    ax.add_line(rad)
 
-rad = Line2D([0, 0], [0, -R], color='red', linewidth=3)
-ax.add_line(rad)
-
-line = Line2D(x, y, color='green', linewidth=3)
-ax.add_line(line)
-
-ax.axis('equal')#устанавливает соотношение сторон масштабирования осей графика равным 1.
-ax.set_aspect('equal', 'box')#устанавливает соотношение сторон масштабирования осей графика так, чтобы график выглядел пропорциональным.
-ax.set(xlim=(-1.5, 14), ylim=(-1, 4))#: устанавливает ограничения для осей x и y, чтобы значения на графике были видны в заданных пределах.
-ax.grid(color='blue', linewidth=0.5, linestyle='dotted')# добавляет на график сетку синего цвета, толщиной 0.5 и пунктирным стилем.
-ax.axhline(y=0, color='k')#добавляет на график горизонтальную линию с y-координатой равной 0.
-ax.axvline(x=0, color='k')#добавляет на график вертикальную линию с x-координатой равной 0.
-ax.set_title('Cycloid-animation')
-
-
-def format_func(value, tick_number):#определяет функцию, которая будет форматировать значения на осях графика.
-    numer = Fraction(value / pi).numerator #вычисляет числитель дроби, полученной при делении значения на pi.
-    denomin = Fraction(value / pi).denominator #вычисляет знаменатель дроби, полученной при делении значения на pi.
-    if numer == 0:
-        return "0"
-    elif numer == 1 and denomin == 1:
-        return r"$\pi$"
-    elif numer == 1 and denomin != 1:
-        return r"$\pi/{0}$".format(denomin)
-    elif numer != 1 and denomin == 1:
-        return r"${0}\pi$".format(numer)
-    else:
-        return r"${0}\pi/{1}$".format(numer, denomin)
+    line = Line2D(x, y, color='green', linewidth=3)
+    ax.add_line(line)
+    ax.axis('equal')  # устанавливает соотношение сторон масштабирования осей графика равным 1.
+    ax.set_aspect('equal', 'box')  # устанавливает соотношение сторон масштабирования осей графика так, чтобы график выглядел пропорциональным.
+    ax.set(xlim=(-5, 20), ylim=(-10, 15))  #: устанавливает ограничения для осей x и y, чтобы значения на графике были видны в заданных пределах.5
+    ax.grid(color='blue', linewidth=0.5,
+            linestyle='dotted')  # добавляет на график сетку синего цвета, толщиной 0.5 и пунктирным стилем.
+    ax.axhline(y=0, color='k')  # добавляет на график горизонтальную линию с y-координатой равной 0.
+    ax.axvline(x=0, color='k')  # добавляет на график вертикальную линию с x-координатой равной 0.
+    ax.set_title('Cycloid-animation')
 
 
-def trans2d(x, y, tx, ty, phi):
-    xx = x * np.cos(phi) - y * np.sin(phi) + tx
-    yy = x * np.sin(phi) + y * np.cos(phi) + ty
-    return (xx, yy)
+    def format_func(value, tick_number):  # определяет функцию, которая будет форматировать значения на осях графика.
+        numer = Fraction(value / pi).numerator  # вычисляет числитель дроби, полученной при делении значения на pi.
+        denomin = Fraction(value / pi).denominator  # вычисляет знаменатель дроби, полученной при делении значения на pi.
+        if numer == 0:
+            return "0"
+        elif numer == 1 and denomin == 1:
+            return r"$\pi$"
+        elif numer == 1 and denomin != 1:
+            return r"$\pi/{0}$".format(denomin)
+        elif numer != 1 and denomin == 1:
+            return r"${0}\pi$".format(numer)
+        else:
+            return r"${0}\pi/{1}$".format(numer, denomin)
 
 
-def init():
-    trace.set_data([], [])
-    rad.set_data([0, 0], [0, -R])
-    line.set_data([], [])
-    return line, rad, trace
+    def trans2d(x, y, tx, ty, phi):
+        xx = x * np.cos(phi) - y * np.sin(phi) + tx
+        yy = x * np.sin(phi) + y * np.cos(phi) + ty
+        return (xx, yy)
 
 
-def get_pos(theta=0):
-    while theta < 4 * pi:
-        ax = np.array([0, 0])
-        by = np.array([0, -R])
-        dx, dy = trans2d(ax, by, theta, 1, -theta)
-        yield theta, dx, dy
-        theta += dtheta
-def animate(pos):
-    theta, dx, dy = pos
-    line.set_data(x + theta, y + 1)
-    rad.set_data(dx, dy)
-    xdata.append(dx[1])
-    ydata.append(dy[1])
-    trace.set_data(xdata, ydata)
-    return line, rad, trace
+    def init():
+        trace.set_data([], [])
+        rad.set_data([0, 0], [0, -R])
+        line.set_data([], [])
+        return line, rad, trace
 
 
-ax.xaxis.set_major_locator(plt.MultipleLocator(pi / 2))#устанавливает основной локатор на оси X, который размещает маркеры на основе заданного интервала, маркеры будут размещены каждые pi / 2 единиц на оси X.
-ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))#устанавливает форматтер для основных маркеров на оси X. Форматтер принимает функцию format_func, которая определяет собственный формат для меток на оси X
+    def get_pos(theta=0):
+        while theta < 4 * pi:
+            ax = np.array([0, 0])
+            by = np.array([0, -R])
+            dx, dy = trans2d(ax, by, theta, 1, -theta)
+            yield theta, dx, dy
+            theta += dtheta
 
 
-myAnimation = animation.FuncAnimation(figu, animate, get_pos, interval=20, blit=True, repeat=False, init_func=init)
+    def animate(pos):
+        theta, dx, dy = pos
+        line.set_data(x + theta, y + 1)
+        rad.set_data(dx, dy)
+        xdata.append(dx[1])
+        ydata.append(dy[1])
+        trace.set_data(xdata, ydata)
+        return line, rad, trace
 
-plt.show()
+
+    ax.xaxis.set_major_locator(plt.MultipleLocator(2))  # устанавливает основной локатор на оси X, который размещает маркеры на основе заданного интервала, маркеры будут размещены каждые 2 единицы на оси X.
+
+    myAnimation = animation.FuncAnimation(figu, animate, get_pos, interval=20, blit=True, repeat=False, init_func=init)
+
+    plt.show()
+else:
+    print("Недопустимое значение радиуса")
